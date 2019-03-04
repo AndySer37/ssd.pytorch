2b0311bb1c2e9a76efff646bdd6d1be166de7c11
AndySer <andy19970307@gmail.com>
2019-03-04 18:11:42 +0800

    revise

--- train_subt.py
+++ train_subt.py
@@ -198,6 +198,7 @@ def train():
         loc_loss += loss_l.data
         conf_loss += loss_c.data
 
+
         if iteration % 10 == 0:
             print('timer: %.4f sec.' % (t1 - t0))
             print('iter ' + repr(iteration) + ' || Loss: %.4f ||' % (loss.data), end='')

a242cd988835a2993824433f867330920cbc2874
AndySer <andy19970307@gmail.com>
2019-03-04 18:10:08 +0800

    revise

--- train_subt.py
+++ train_subt.py
@@ -195,15 +195,15 @@ def train():
         loss.backward()
         optimizer.step()
         t1 = time.time()
-        loc_loss += loss_l.data[0]
-        conf_loss += loss_c.data[0]
+        loc_loss += loss_l.data
+        conf_loss += loss_c.data
 
         if iteration % 10 == 0:
             print('timer: %.4f sec.' % (t1 - t0))
-            print('iter ' + repr(iteration) + ' || Loss: %.4f ||' % (loss.data[0]), end='')
+            print('iter ' + repr(iteration) + ' || Loss: %.4f ||' % (loss.data), end='')
 
         if args.visdom:
-            update_vis_plot_iter(viz, iteration, loss_l.data[0], loss_c.data[0],
+            update_vis_plot_iter(viz, iteration, loss_l.data, loss_c.data,
                             iter_plot, 'append')
 
         if iteration != 0 and iteration % 1000 == 0:
